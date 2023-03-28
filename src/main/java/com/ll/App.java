3b2c1c9e1092585381272cb7d2c44802b6ad6754
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-28 12:42:10 +0900

    1~2단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -8,15 +8,27 @@ public class App {
 
     public App(Scanner sc) {
         this.sc = sc;
+
     }
 
     public void run() {
         System.out.println("== 명언 앱 ==");
-        while(true){
+
+        while (true) {
             System.out.println("명령)");
-            String command = sc.nextLine();
-            if(command.equals("종료")) {
+            String command = sc.nextLine().trim();
+
+            if (command.equals("종료")) {
                 break;
+            }else if(command.equals("등록")){
+
+                System.out.println("명언 : ");
+                String wise = sc.nextLine().trim();
+
+                System.out.println("작가 : ");
+                String name = sc.nextLine().trim();
+
+                System.out.println("1번 명언이 등록되었습니다.");
             }
         }
     }
